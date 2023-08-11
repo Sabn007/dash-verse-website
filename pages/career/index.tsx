dba08c8f7fc2879e988ba65b0cb2ad3ac12b73b0
Sabin Maharjan <samah013@kathmandu>
2023-08-11 08:29:20 +0545

    fix: tailwinds refactor

--- pages/career/index.tsx
+++ pages/career/index.tsx
@@ -1,7 +1,7 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import React from "react";
-import "../../Globals.css";
+import "../../globals.css";
 
 const CareerPage: React.FC = () => {
   const jobVacancies = [
